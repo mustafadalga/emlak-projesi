8f6a0ef9b9cba70cb21d7b0dd6d05866c636841e
Mustafa Dalga <25087769+mustafadalga@users.noreply.github.com>
2019-08-21 21:43:24 +0300

    Update local_settings.py

--- btre/local_settings.py
+++ btre/local_settings.py
@@ -20,6 +20,6 @@ DATABASES = {
 #Email config
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
-EMAIL_HOST_USER="djangoicin@gmail.com"
-EMAIL_HOST_PASSWORD="testamacli"
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=""
 EMAIL_USE_TLS=True
